6c4ebeb728960143dd5e1f83171485347331c62a
Andrew Schleifer <me@andrewschleifer.name>
2010-03-24 23:33:02 -0500

    Refactored the book class for better readability.

--- book.rb
+++ book.rb
@@ -1,26 +1,20 @@
 require 'osx/cocoa'
 
-DEFAULT_RATE = 3.0
-
 class Book < OSX::NSDocument
   ib_action :change_speed
   ib_action :start_stop
   ib_outlet :button, :slider, :word_view
   kvc_accessor :index, :playing, :rate, :timer, :words
 
-  def awakeFromNib
+  def initialize
     @index = 0
     @playing = false
-    @rate = DEFAULT_RATE
+    @rate = 3.0
     @timer = nil
-    @words ||= File.read(File.join(
-      OSX::NSBundle.mainBundle.resourcePath.fileSystemRepresentation,
-      'Hamlet.txt')).scan(/\S+/)
+    @words = []
+  end
 
-    @button.setTitle ">"
-    @slider.minValue = -5
-    @slider.maxValue = 10
-    @slider.numberOfTickMarks = @slider.maxValue - @slider.minValue + 1
+  def awakeFromNib
     @slider.floatValue = @rate
     @word_view.stringValue = @words[@index]
   end
@@ -35,32 +29,32 @@ class Book < OSX::NSDocument
   end
 
   def start_stop
-    if @playing
-      @timer.invalidate
-      @button.setTitle ">"
-    else
-      start_a_timer
-  		@button.setTitle "||"
-    end
     @playing = ! @playing
+
+    if @playing
+      @button.setTitle "Play"
+      start_the_timer
+    else
+      @button.setTitle "Pause"
+      @timer.invalidate
+    end
   end
 
   def change_speed
     @rate = @slider.floatValue
-    start_a_timer
+    if @playing
+      @timer.invalidate
+      start_the_timer
+    end
   end
 
   def advance
-    if @playing
-      @index += @rate / @rate.abs
-      @word_view.stringValue = @words[@index]
-    end
+    return unless @playing
+    @index += @rate / @rate.abs
+    @word_view.stringValue = @words[@index]
   end
 
-  def start_a_timer
-    if @timer
-      @timer.invalidate
-    end
+  def start_the_timer
     @timer = OSX::NSTimer.objc_send(
       :scheduledTimerWithTimeInterval, 1 / @rate.abs,
       :target, self,
@@ -68,4 +62,5 @@ class Book < OSX::NSDocument
       :userInfo, nil,
       :repeats, true)
   end
+
 end
