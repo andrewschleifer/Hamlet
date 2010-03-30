86f6601cf5597443e6734c11159f316682ef29c6
Andrew Schleifer <me@andrewschleifer.name>
2010-03-29 23:45:07 -0500

    Added credits to About...
    * created credits file
    * added credits file to build
    * new build version
    * removed TODO and FIXME

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ BUNDLEID = "com.mutant-atomic.#{NAME}"
 RUBYFILES = ['main.rb', 'book.rb', 'delegate.rb', 'window.rb']
 COMPILED = ['main.m', 'WordView.m']
 FRAMEWORKS = ['RubyCocoa', 'Cocoa']
-RESOURCES = ['main.nib', 'book.nib', 'preferences.nib',
+RESOURCES = ['main.nib', 'book.nib', 'preferences.nib', 'Credits.html',
   'play.png', 'pause.png', 'Hamlet.txt']
