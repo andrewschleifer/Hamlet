86f7673d330c77d763855736361774c466adff4c
Andrew Schleifer <me@andrewschleifer.name>
2010-03-29 21:41:13 -0500

    Set default colors at start-up * set nsuserdefaults for foregroundcolor to black * set nsuserdefaults for backgroundcolor to white * new build version * removed FIXME

--- main.rb
+++ main.rb
@@ -8,5 +8,13 @@ Pathname.glob(path + '*.rb') do |file|
 	require(file)
 end
 
+{"foregroundColor" => OSX::NSColor.blackColor,
+  "backgroundColor" => OSX::NSColor.whiteColor}.each do |name, color|
+  unless OSX::NSUserDefaults.standardUserDefaults.dataForKey(name)
+    OSX::NSUserDefaults.standardUserDefaults.setObject_forKey(
+      OSX::NSArchiver.archivedDataWithRootObject(color), name)
+  end
+end
+
 OSX::NSApplication.sharedApplication
 OSX.NSApplicationMain(0, nil)
