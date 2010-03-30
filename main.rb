fd617494f138a549248207d07f391c9f09d14fa0
Andrew Schleifer <me@andrewschleifer.name>
2010-03-29 21:02:02 -0500

    Removed a vendor function we don't use.

--- main.rb
+++ main.rb
@@ -1,12 +1,6 @@
 require 'osx/cocoa'
 require 'pathname'
 
-def log(*args)
-	args.each do |m|
-		OSX.NSLog m.inspect
-	end
-end
-
 path = Pathname.new OSX::NSBundle.mainBundle.resourcePath.fileSystemRepresentation
 
 Pathname.glob(path + '*.rb') do |file|
